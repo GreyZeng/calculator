c3324838f1a4b656bff833e1b9473dcf29554615
GreyZeng <410486047@qq.com>
2017-09-22 22:27:19 +0800

    init unit test environment

--- src/main/java/com/cp/App.java
+++ src/main/java/com/cp/App.java
@@ -2,5 +2,11 @@ package com.cp;
 
 
 public class App {
+    public static void main(String[] args) {
+        generate();
+    }
 
+    public static String generate() {
+        return "";
+    }
 }
