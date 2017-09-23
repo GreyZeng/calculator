107eae7d225c5d078e2b03a119f2d37789b4d837
GreyZeng <410486047@qq.com>
2017-09-23 16:40:56 +0800

    generate expression:  divide 0 is forbid

--- src/main/java/com/cp/core/impl/Generator.java
+++ src/main/java/com/cp/core/impl/Generator.java
@@ -5,9 +5,11 @@ import com.cp.core.Expression;
 import com.cp.core.IGenerator;
 import com.google.common.base.Joiner;
 
+import java.util.Arrays;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static com.google.common.base.Objects.equal;
 import static com.google.common.collect.Sets.newHashSet;
 import static org.apache.commons.lang3.StringUtils.SPACE;
 
@@ -20,6 +22,8 @@ public class Generator implements IGenerator {
     @Override
     public Set<Expression> generate(Config config) {
 
+        System.out.println(config);
+        System.out.println("Start generate expression...\n");
         int numberOfExpression = config.getNumberOfExpression();
         Set<Expression> set = newHashSet();
         for (int i = 0; i < numberOfExpression; i++) {
@@ -32,15 +36,24 @@ public class Generator implements IGenerator {
             int numberOfOpand = randomNumberOfOperation + 1;
             String[] exp = new String[randomNumberOfOperation + numberOfOpand];
             for (int j = 0; j < randomNumberOfOperation + numberOfOpand; j++) {
-                if (j % 2 == 1) {
-                    exp[j] = pickAnOperation(config.generateAvailableOperators());
-                } else {
+                if (j % 2 == 0) {
                     exp[j] = generateOprand(config.isHasFraction(), config.getRange());
+                    if (j > 0 && equal(exp[j - 1], "÷") && equal(exp[j], "0")) {
+                        // could not be ÷ 0 , re generator operator from + - x
+                        exp[j - 1] = pickAnOperation(config.generateAvailableOperators(), "÷");
+                    }
+                } else {
+                    exp[j] = pickAnOperation(config.generateAvailableOperators());
                 }
             }
             set.add(Expression.create(Joiner.on(SPACE).join(exp)));
         }
         return set;
+    }
+
+    private String pickAnOperation(String[] operators, String except) {
+        String[] newOperators = Arrays.stream(operators).filter(operator -> !equal(operator, except)).toArray(String[]::new);
+        return pickAnOperation(newOperators);
     }
 
     private String pickAnOperation(String[] operators) {
@@ -51,4 +64,5 @@ public class Generator implements IGenerator {
         // TODO handle fraction
         return String.valueOf(ThreadLocalRandom.current().nextInt(0, range + 1));
     }
+
 }
