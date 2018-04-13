fb698ae614aef272ebe240a5ed82e6f1329c8e62
GreyZeng <410486047@qq.com>
2018-04-13 21:49:38 +0800

    post请求

--- src/main/java/com/hui/calculator/controllers/CheckController.java
+++ src/main/java/com/hui/calculator/controllers/CheckController.java
@@ -2,10 +2,12 @@ package com.hui.calculator.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.hui.calculator.core.Checker.errorResult;
 import static com.hui.calculator.core.Checker.getCheckedInfo;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * @author zenghui
@@ -13,7 +15,9 @@ import static com.hui.calculator.core.Checker.getCheckedInfo;
 @RestController
 public class CheckController {
 
-    @RequestMapping("/checking")
+
+    @RequestMapping(value = "/checking", method = POST)
+    @ResponseBody
     public String checking(@RequestParam(value = "answer") String answer) {
         try {
             return getCheckedInfo(answer);
